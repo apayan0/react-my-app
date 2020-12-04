289e5fa703151b838dd38f424a553ae7140e39a1
apayan0 <apayan13@gmail.com>
2020-12-03 22:51:06 -0800

    First commit.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! My name is Arturo!
+        </p>
+        <p>
+          I want more Bundt cake
         </p>
         <a
           className="App-link"
